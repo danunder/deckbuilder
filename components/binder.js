6cb74c40bf419573f9361b33f94248caef98c69c
Daniel Robertson <danieljrobertson@gmail.com>
2021-03-08 19:33:07 -0500

    hook up react card binder component

---
+++ components/binder.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import Binder, { Card, Page } from 'card-binder'
+ 
+export default function MyBinder(props) {
+  return (
+    <Binder>
+      <Page>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+      
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+      
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+        <Card standard>
+          <h2>Standard Card</h2>
+        </Card>
+      </Page>
+    </Binder>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Binder from '../components/binder'
+
+
+export default function Home() {
+  return (
+    <Binder>
+
+    </Binder>
+  )
+}
