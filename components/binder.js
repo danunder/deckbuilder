adab427e4ffc058adaa2bad7bcfbc8e6f51139e4
Daniel Robertson <danieljrobertson@gmail.com>
2021-03-08 21:25:57 -0500

    set up card data and get images displaying on screen

--- components/binder.js
+++ components/binder.js
@@ -1,48 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
 import Binder, { Card, Page } from 'card-binder'
  
 export default function MyBinder(props) {
+
+  const cardData = require('./cardData.json')
+  const sampleCardData = cardData.filter((x, i) => i < 10 )
+
+  const getImgUrl = (code) => `https://definitelynotswdestinycardimages.s3.us-east-2.amazonaws.com/${code}.jpg`
+
+  const cards = cardData.map(card => {
+    return (
+      <Card
+        key={card.code}
+        name={card.name}
+      >
+        <Image
+          src={getImgUrl(card.code)}
+          height='420'
+          width='300'
+        ></Image>
+      </Card>
+    )
+  });
+
   return (
     <Binder>
       <Page>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-      
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-      
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
-        <Card standard>
-          <h2>Standard Card</h2>
-        </Card>
+        {cards}
       </Page>
     </Binder>
   )
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ["definitelynotswdestinycardimages.s3.us-east-2.amazonaws.com"],
+  },
+}
